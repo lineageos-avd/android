eb3911f215f5dc360ac8f2f5fd146cfc594ae9db
Xingzhi Li / 李幸值 <moeleaking@gmail.com>
2026-09-05 08:58:06 +0800

    Build SDK image ZIPs through Goldfish emu_img_zip target
    The original Lab build graph maps emu_img_zip to sdk-repo-linux-system-images.zip. The older sdk_addon goal references a removed advancedFeatures.ini.arm and is not the producer of the imported images.

--- scripts/build-system.sh
+++ scripts/build-system.sh
@@ -10,7 +10,7 @@ source build/envsetup.sh
 for arch in arm64 x86_64; do
   export OUT_DIR="out/avd-$arch"
   lunch "lineage_sdk_phone_${arch}-bp4a-userdebug"
-  m -j"${BUILD_JOBS:-32}" sdk_addon
+  m -j"${BUILD_JOBS:-32}" emu_img_zip
   case "$arch" in arm64) product=emu64a;abi=arm64-v8a;; x86_64) product=emu64x;abi=x86_64;; esac
   cp "$OUT_DIR/target/product/$product/sdk-repo-linux-system-images.zip" "$output/lineage-23.2-api36.1-$abi.zip"
 done
